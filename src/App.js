5ae6029b8e48964bc95d7a9ca359adab0ab7c682
Salavdor <SalvadorRamon20@gmail.com>
2024-04-17 22:07:57 -0500

    Updates some asthetics.

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       </h3>
       <TextField size='small' value={inputText} onChange={handleChange} sx={{
         '& .MuiOutlinedInput-root': {
-          backgroundColor: 'lightgrey',
+          backgroundColor: 'red',
           fontWeight: 'bold',
           fontFamily: 'Noto Serif',
         },
